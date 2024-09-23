b67b8f7ec0f468fc92ee30184ecaf612336a03e2
henriqueda.silva <henriqueda.silva@dataprev.gov.br>
2024-09-23 14:57:29 -0300

    status no swagger adicionados ao endpoint de login, adicionar apos configurar o security no endpoint de testautorization

--- src/main/java/com/henriquesv/simpleauthent/controller/SaJsController.java
+++ src/main/java/com/henriquesv/simpleauthent/controller/SaJsController.java
@@ -2,10 +2,17 @@ package com.henriquesv.simpleauthent.controller;
 
 import com.henriquesv.simpleauthent.entity.SaJsEntity;
 import com.henriquesv.simpleauthent.service.SajsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Book;
 
 @RestController
 @RequestMapping("/api")
@@ -14,6 +21,12 @@ public class SaJsController {
     @Autowired
     SajsService sajsService;
 
+    @Operation(summary = "Operação de login que retorna uma jwt")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "login feito com sucesso",
+                    content =  @Content),
+            @ApiResponse(responseCode = "404", description = "username ou password incorreto",
+                    content =  @Content) })
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody SaJsEntity authEntity){
 
